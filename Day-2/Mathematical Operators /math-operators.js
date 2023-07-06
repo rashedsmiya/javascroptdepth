16525285ab5d95d36e54dbf2bfe209d057526adf
rashedsmiya <rashedmiyaofficial@gmail.com>
2023-07-06 14:39:35 +0600

    ptag-added

---
+++ Day-2/Mathematical Operators /math-operators.js
@@ -0,0 +1,26 @@
+// Addition
+
+let age = 10 + 23;
+console.log(age); // 33
+
+// Subtraction
+
+let points = 50 - 19;
+console.log(points);
+
+// Multiplication
+let score = 7 * 5;
+console.log(points);
+
+// Division
+let divide = 100 / 13;
+console.log(divide);
+
+// Modulus (Remainder)
+let mod = 12 % 4;
+console.log(mod);
+
+// Exponentiation
+let exp = 4 ** 3; 
+console.log(exp);
+
