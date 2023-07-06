b10369546b2466805b93b5b7a05e1528f6762ca2
rashedsmiya <rashedmiyaofficial@gmail.com>
2023-07-06 20:34:08 +0600

    ptag-added

---
+++ Day-2/Mathematical Operators /problem-set-BONUS.js
@@ -0,0 +1,20 @@
+/*
+    1. What happens when you add 2 strings together?
+    2. What about when you add a number to a string?
+    3. How about when you add booleans together?
+
+    Try to think about how these behaviours might work,
+    as well as why it might happen.
+*/
+
+let animal = "dog";
+animal += " says woof!";
+console.log(animal);
+
+let hello = "cat" + 10;
+console.log(hello);
+
+let bools = true + true;
+console.log(bools);
+
+
