cea90e6988f16ba9b6d0563a858ef29dd59426d3
rashedsmiya <rashedmiyaofficial@gmail.com>
2023-07-07 22:40:24 +0600

    commit -m ptag-added

---
+++ Boolean Operators/boolean-assignment.js
@@ -0,0 +1,18 @@
+const fruit = true && "bananas";
+console.log(fruit);
+
+confirm.log(false && "oranges");
+console.log(0 && "cucumbers");
+
+console.log("---------");
+
+console.log(true || "bananas");
+console.log(false || "oranges");
+console.log("" || "cucumbers");
+
+
+console.log("--------");
+
+console.log(0 ?? "monkeys");
+console.log("" ?? "koalas");
+console.log(null ?? "elephants");
---
+++ problem-set-BONUS.js
@@ -0,0 +1,36 @@
+/*
+    First, lookup how logical AND, OR and NULLISH assignment work
+    using Google. Then try the following examples.
+
+    What would each of the following variables evaluate to:
+
+    1. let username = "";
+       username &&= "tiger";
+
+    2. let vip = 0;
+       vip ||= 3;
+
+    3. let loggedIn = null;
+       loggedIn ??= 0;
+
+    Log each of the values out to see the results.
+
+    This is very very challenging, it might take several tries
+    or coming back to it in the future when it's more relevant.
+*/
+
+let username = undefined;
+username &&= 5; // username = username && "tiger"
+// username = "" && "tiger"
+console.log(username);
+
+let vip = undefined;
+vip ||= 3; // vip = vip || 3 vip = 0 || 3
+console.log(vip);
+
+let loggedIn = typeof 10;
+loggedIn ??= 5; 
+console.log(loggedIn);
+
+
+
