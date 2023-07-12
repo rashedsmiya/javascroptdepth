adab2878e000a98dcaae7d9d4627c044157e5598
rashedsmiya <rashedmiyaofficial@gmail.com>
2023-07-12 20:36:56 +0600

    ptag-added

---
+++ Functions/functions.js
@@ -0,0 +1,12 @@
+// function declaration
+const sayHello = () => {
+    console.log("Hello, Jello!");
+    console.log("meow!");
+};
+
+
+// "Calling" the function
+sayHello("moo!");
+sayHello("foo!");
+sayHello();
+sayHello();
